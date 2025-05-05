aff4cde0d42f6518d74856709e4ed3d4e691bc26
ivanRODRIGO125 <ivanrodrigolasala@gmail.com>
2025-05-05 20:34:11 -0300

    error de canvas resuelto
    error de canvas resuelto

--- Assets/Scripts/color del escenario/PlataformasAzules.cs
+++ Assets/Scripts/color del escenario/PlataformasAzules.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlataformasAzules : MonoBehaviour
 {
-    private Renderer Plataforma;
+    public Renderer Plataforma;
     // Start is called before the first frame update
     private void Start()
     {
         Plataforma = GetComponent<Renderer>();
-
+        Plataforma.material.color = Color.blue;
     }
     // Update is called once per frame
     void Update()
     {
-        Plataforma.material.color = Color.blue;
+        //Plataforma.material.color = Color.blue;
     }
 }
